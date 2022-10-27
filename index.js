25b86607dadb13ddaeb0c3701d7694f6895694b4
PedroErath <pverath@gmail.com>
2022-10-27 09:26:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const userRoute = require('./Routes/userRoute');
 const authRoute = require('./Routes/authRoute');
 const requestRoute = require('./Routes/requestRoute');
+const cors = require('cors')
 require('dotenv').config();
 
 /* Connection with mongoDB */
@@ -13,6 +14,14 @@ mongoose.connect(process.env.DATABASE).then(() => {
 }).catch((error) => {
     console.log(error);
 })
+
+/* Enable cors for client requests */
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+    app.use(cors());
+    next();
+});
 
 /* pattern route for user manipulation */
 app.use('/user', userRoute);
