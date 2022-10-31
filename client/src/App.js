09e35465bbf559a13223cd7bd72917c0334c4cac
PedroErath <pverath@gmail.com>
2022-10-31 11:40:03 -0300

    Token Verify App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
-import React from "react";
-import Login from './components/Login'
-/* import Home from "./components/Home"; */
+import React, { useEffect, useState } from "react";
+import Login from './components/Login';
+import Home from "./components/Home";
 
 
 function App() {
+
+  const [logged, setLogged] = useState({});
+
+  useEffect(() => {
+    fetch('http://localhost:3001/auth/verifytoken', {
+      method: 'GET',
+      headers: {
+        authorization: `Bearer ${localStorage.getItem('authorization')}`
+      }
+    })
+      .then(response => response.json())
+      .then(response => {
+        response.success ? setLogged(response) : setLogged({success: false})
+      })
+      .catch(err => console.error(err));
+  }, [])
+
   return (
     <div className="">
-        <Login />
+      {
+        logged.success ? <Home /> : <Login />
+      }
     </div>
   );
 }
